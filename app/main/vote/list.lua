ef0079a1c2cd97ea54722ee40685ad267f5fd0ca
lava <lava@gruss.cc>
2013-12-16 11:17:29 +0100

    preparations for voting delegations

--- app/main/vote/list.lua
+++ app/main/vote/list.lua
@@ -150,7 +150,26 @@ for i = 1, max_grade do
   end
 end
 
+--[[
+-- voting delegations
+local members_selector = Member:new_selector()
+:join("delegating_voter", nil, "delegating_voter.member_id = member.id")
+:add_where{ "delegating_voter.issue_id = ?", issue.id }
+:add_where{ "delegating_voter.delegate_member_ids[1] = ?", member.id }
+:add_field("delegating_voter.weight", "voter_weight")
+:join("issue", nil, "issue.id = delegating_voter.issue_id")
 
+execute.view{
+  module = "member",
+  view = "_list",
+  params = {
+    members_selector = members_selector,
+    initiative = initiative,
+    trustee = member,
+    for_votes = true
+  }
+}
+--]]--
 
 if not readonly then
   util.help("vote.list", _"Voting")
