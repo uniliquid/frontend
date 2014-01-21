2339435784523638a093cda58a0fb6b6b0bc29f6
lava <lava@gruss.cc>
2014-01-21 20:35:12 +0100

    contacts for invite initiator (illionas)

--- app/main/initiative/add_initiator.lua
+++ app/main/initiative/add_initiator.lua
@@ -44,22 +44,47 @@ ui.form{
         name = _"Choose member"
       }
     }
-local contact_members = Member:build_selector{
---  is_contact_of_member_id = app.session.member_id,
-  voting_right_for_unit_id = voting_right_unit_id,
-  active = true,
-  order = "name"
-}:exec()
---    local contact_members = app.session.member:get_reference_selector("saved_members"):add_order_by("name"):exec()
-    for i, record in ipairs(contact_members) do
-      records[#records+1] = record
+
+    local all_members = Member:build_selector{
+      voting_right_for_unit_id = voting_right_unit_id,
+      active = true,
+      locked = false,
+      order = "name"
+    }:exec()
+
+    local contact_members = Member:build_selector{
+      is_contact_of_member_id = app.session.member_id,
+      voting_right_for_unit_id = voting_right_unit_id,
+      active = true,
+      locked = false,
+      order = "name"
+    }:exec()
+
+    -- add saved members
+    if #contact_members > 0 then
+      records[#records+1] = {id="_", name= "--- " .. _"Saved contacts" .. " ---"}
+      for i, record in ipairs(contact_members) do
+        records[#records+1] = record
+      end
     end
+    -- add all members
+    if #all_members > 0 then
+      records[#records+1] = {id="_", name= "--- " .. _"All members" .. " ---"}
+      for i, record in ipairs(all_members) do
+        records[#records+1] = record
+      end
+    end
+    disabled_records = {}
+    disabled_records["_"] = true
+    disabled_records[app.session.member_id] = true
+
     ui.field.select{
       label = _"Member",
       name = "member_id",
       foreign_records = records,
       foreign_id = "id",
-      foreign_name = "name"
+      foreign_name = "name",
+      disabled_records = disabled_records
     }
     ui.submit{ text = _"Save" }
   end
